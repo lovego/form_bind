d6c6890b450ebe289f8bf04c47d1e605a622b723
廖恒晨 <liaohengchen@gmail.com>
2018-12-12 16:24:14 +0800

    remove import xiaomei/utils

--- form_test.go
+++ form_test.go
@@ -4,16 +4,14 @@ import (
 	"net/url"
 	"reflect"
 	"testing"
-
-	"github.com/lovego/xiaomei/utils"
 )
 
 type SubData struct {
-	Field4 bool   `form:"field4"`
-	Field5 bool   `form:"field5,default=true"`
-	Field6 bool   `form:"field6"`
-	Field7 bool   `form:"field7"`
-	Field8 string `form:"field8,default=sdf"`
+	Field4 bool     `form:"field4"`
+	Field5 bool     `form:"field5,default=true"`
+	Field6 bool     `form:"field6"`
+	Field7 bool     `form:"field7"`
+	Field8 string   `form:"field8,default=sdf"`
 	Field9 []string `form:"field9"`
 }
 
@@ -39,7 +37,7 @@ func TestFormMapping1(t *testing.T) {
 			Field8: "sdf",
 		},
 	}
-	utils.PrintJson(qd)
+	t.Log(qd)
 	if !reflect.DeepEqual(qd, expected) {
 		t.Fatalf("unexpected queryData")
 	}
@@ -75,9 +73,6 @@ func TestFormMapping2(t *testing.T) {
 		},
 	}
 	if !reflect.DeepEqual(expected, qd) {
-		utils.PrintJson(qd)
 		t.Fatalf("unexpected queryData")
 	}
 }
-
-
